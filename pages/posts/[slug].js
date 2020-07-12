93add0feb5ac636f2c1e0ed3de2f4596fec38e54
Goto Hayato <habita.gh@gmail.com>
2020-07-12 17:43:27 +0900

    投稿の詳細ページを `Layout` を利用して作成。

---
+++ pages/posts/[slug].js
@@ -0,0 +1,39 @@
+import fs from "fs"
+import path from "path"
+
+import Layout from "../../components/Layout"
+import { listContentFiles, readContentFile } from "../../lib/contentLoader"
+
+export default function Post(params) {
+  return (
+    <Layout title={params.title}>
+      <div className="post-meta">
+        <span>{params.published}</span>
+      </div>
+      <div className="post-body"
+        dangerouslySetInnerHTML={{ __html: params.content }}
+      />
+    </Layout>
+  )
+}
+
+export async function getStaticProps({ params }) {
+  const content = await readContentFile({ fs, slug: params.slug })
+
+  return {
+    props: {
+      ...content
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const paths = listContentFiles({ fs })
+    .map((filename) => ({
+      params: {
+        slug: path.parse(filename).name,
+      }
+    }))
+
+  return { paths, fallback: false }
+}
