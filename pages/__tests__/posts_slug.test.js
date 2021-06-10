eb506a7e4996680575edd6442ea643cafc8c9183
Goto Hayato <habita.gh@gmail.com>
2021-06-10 12:38:40 +0900

    Add some tests for page components.

---
+++ pages/__tests__/posts_slug.test.js
@@ -0,0 +1,32 @@
+import { cloneElement } from "react"
+import renderer from "react-test-renderer"
+
+import Layout from "../../components/Layout"
+import Post from "../posts/[slug].js"
+
+jest.mock("../../components/Layout")
+
+describe("Post", () => {
+	Layout.mockImplementation(({ title, children }) => (
+		<div>
+			<span>{title}</span>
+			{children}
+		</div>
+	))
+
+	it("renders correctly", () => {
+		const content = [
+			"## 見出し 2",
+			"",
+			"昔々あるところに。",
+		].join("\n")
+		const tree = renderer
+			.create(<Post 
+				title="はじめての投稿" 
+				published="2030/10/20" 
+				content={content} 
+			/>)
+			.toJSON()
+		expect(tree).toMatchSnapshot()
+	})
+})
