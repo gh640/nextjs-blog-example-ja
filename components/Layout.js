a98557c74c01e502144b2390706c41beb0bd0e64
Goto Hayato <gh640@hey.com>
2023-02-08 10:47:47 +0900

    Issue #187: Google Fonts フォントの利用に `@next/font` を使う。

--- components/Layout.js
+++ components/Layout.js
@@ -64,20 +64,12 @@ const Layout = (props) => {
         body {
           padding: 0;
           margin: 0;
-          font-family: 'Noto Sans JP', -apple-system, "Segoe UI", "Helvetica Neue",
-            "Hiragino Kaku Gothic ProN", メイリオ, meiryo, sans-serif;
           color: #222;
-          font-size: 16px;
         }
 
         img,
         iframe {
           max-width: 100%;
-        }
-
-        h1, h2, h3, h4, h5, h6 {
-          font-family: Montserrat, -apple-system, "Segoe UI", "Helvetica Neue",
-            "Hiragino Kaku Gothic ProN", メイリオ, meiryo, sans-serif;
         }
 
         /* Prism のコードブロックに影響を与えず <code> にスタイルを追加 */
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
 import Router from 'next/router'
-
+import { Montserrat, Noto_Sans_JP } from '@next/font/google'
 import { GA_TRACKING_ID, pageview } from '../lib/gtag'
+
+/*
+ * Google Fonts を利用する
+ *
+ * - Montserrat
+ * - Noto Sans JP
+ */
+const montserrat = Montserrat({ subsets: ['latin'] })
+const noto_sans_jp = Noto_Sans_JP({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  fallback: ['-apple-system', 'Segoe UI', 'Helvetica Neue'],
+})
 
 if (GA_TRACKING_ID) {
   Router.events.on('routeChangeComplete', url => pageview(url))
@@ -8,5 +21,21 @@ if (GA_TRACKING_ID) {
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${noto_sans_jp.style.fontFamily};
+            "Hiragino Kaku Gothic ProN", メイリオ, meiryo, sans-serif;
+          font-size: 16px;
+        }
+
+        h1, h2, h3, h4, h5, h6 {
+          font-family: ${montserrat.style.fontFamily},
+            "Hiragino Kaku Gothic ProN", メイリオ, meiryo, sans-serif;
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
